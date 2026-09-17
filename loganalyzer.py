1adfc5c95f864349b4c87248e1dedc5fccf81196
Adeel "Stoic" Asghar <264592599+adeelasghar12123-lang@users.noreply.github.com>
2026-09-17 21:58:32 +0500

    sales data analyzer init

--- loganalyzer.py
+++ loganalyzer.py
@@ -1,12 +1,8 @@
 list_of_dic = []
-log_level_dic = {
-    "ERROR" : 0,
-    "INFO" : 0,
-    "WARNING" : 0
-}
+log_level_dic = {}
 log_list = []
 
-def load_log(list_of_dic = list_of_dic, log_list = log_list):
+def load_log(list_of_dic , log_list ):
     try:
         with open("app.log","r") as file:
             for line in file:
@@ -14,7 +10,6 @@ def load_log(list_of_dic = list_of_dic, log_list = log_list):
     except FileNotFoundError:
         print("File not found !")
         return
-    
     if log_list:
         for line in log_list:
             date,time,message = line.split(" ",2)
@@ -24,20 +19,22 @@ def load_log(list_of_dic = list_of_dic, log_list = log_list):
                 "Message" : message
             })
 
-def log_level(log_level_dic = log_level_dic, list_of_dic = list_of_dic):
-    load_log()
+def log_level(log_level_dic , list_of_dic ):
+    load_log(list_of_dic , log_list)
     for dic in list_of_dic:
-        message = dic["Message"]
-        parts = message.split(" ",1)
+        parts = dic["Message"].split(" ",1)
         log_type = parts[0]
-        if log_type in log_level_dic:
+        if log_type in log_level_dic.keys():
             log_level_dic[log_type] += 1
+        else:
+            log_level_dic[log_type] = 1
 
-    print(f'ERROR : {log_level_dic["ERROR"]}')
-    print(f'INFO : {log_level_dic["INFO"]}')
-    print(f'WARNING : {log_level_dic["WARNING"]}')
+    print("=== OCCURANCES ===")
+    for key,value in log_level_dic.items():
+        print(f'{key} : {value}')
 
-def display_errors(list_of_dic = list_of_dic):
+
+def display_errors(list_of_dic ):
     print("=== ERRORS ===")
     for dic in list_of_dic:
         parts = dic["Message"].split(" ",1)
@@ -45,35 +42,35 @@ def display_errors(list_of_dic = list_of_dic):
         if log_type == "ERROR":
             print(dic["Message"])
 
-
-def common_level(list_of_dic = list_of_dic , log_level_dic = log_level_dic):
+def common_level(list_of_dic , log_level_dic ):
     max_count = max(log_level_dic.values())
-    if max_count == 0:
-        print("No logs in file.")
+    if not log_level_dic:
+        print("No logs available to count.")
         return
-    max_logs = []
+    print("Most common occurances !")
     for log_type,count in log_level_dic.items():
         if count == max_count:
-            max_logs.append(log_type)
-    print(f'OCCURRANCES : {max_count}')
-    for log in max_logs:
-        print(f'Most common log : {log}')
+            print(f'{log_type} : {count}')
 
 
-def search_log(list_of_dic = list_of_dic, log_list = log_list):
+
+def search_log(list_of_dic, log_list):
     to_search = input("Search Log : ").lower().strip()
     for line in log_list:
-        if to_search in line.lower().strip():
-            print(line)
+        parts = line.split(" ",2)
+        message = parts[2]
+        if to_search in message.lower().strip():
+            print(message)
 
-def log_summary(list_of_dic = list_of_dic , log_level_dic = log_level_dic , log_list = log_list):
+
+def log_summary(list_of_dic , log_level_dic  , log_list ):
     print(f' Total log entries : {len(log_list)}')
     for key,value in log_level_dic.items():
         print(f'{key} : {value}')
-        common_level()
+    common_level(list_of_dic , log_level_dic )
 
 
-log_level()
-display_errors()
-search_log()
-log_summary()
+log_level(log_level_dic , list_of_dic )
+display_errors(list_of_dic)
+log_summary(list_of_dic , log_level_dic  , log_list)
+search_log(list_of_dic, log_list)
