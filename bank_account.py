f7337df6c42dd7293876451e543b35c63e5ff2de
Adeel "Stoic" Asghar <264592599+adeelasghar12123-lang@users.noreply.github.com>
2026-09-19 13:28:07 +0500

    bank_accounts

---
+++ bank_account.py
@@ -0,0 +1,66 @@
+class bank_account:
+    def __new__(cls, name, balance ):
+        if type(name) is not str or balance < 0:
+            print("Enter valid details ! Account not created")
+            return None
+        return super().__new__(cls)
+
+    def __init__(self, name, balance):
+        self.name = name 
+        self. balance = balance
+        self.transaction = []
+
+
+
+    def withdraw(self,amount):
+        if amount > self.balance:
+            print("Not enough balance !")
+            return
+        if amount < 1:
+            print("Enter a valid amount to withdraw !")
+            return
+        self.balance -= amount
+        self.transaction.append({"Withdrew" : amount})
+        print(f"{amount} successfully withdrew from {self.name}'s account ! Current balance is {self.balance}")
+
+
+
+    def deposit(self, amount):
+        if amount < 1 :
+            print("Enter a valid amount to deposit !")
+            return
+        self.balance += amount
+        self.transaction.append({"Deposited" : amount})
+        print(f"{amount} successfully deposited in {self.name}'s account ! Current balance is {self.balance}")
+
+
+    def get_balance(self):
+        return self.balance
+
+    def show_transactions(self):
+        if not self.transaction:
+            print("No transactions made yet !")
+        else:
+            for trans in self.transaction:
+                for key,value in trans.items():
+                    print(key,value)
+
+    def show_summary(self):
+        print("==== ACCOUNT SUMMARY ====")
+        print(f'Account holder : {self.name}')
+        print(f'Balance : {self.balance}')
+        print(f'Total Transactions : {len(self.transaction)}')
+        print("==========================")
+
+
+acc1 = bank_account("Dawood",100)
+
+print(f'Balance : {acc1.get_balance()}')
+
+acc1.withdraw(10)
+acc1.deposit(5)
+acc1.withdraw(54)
+acc1.show_transactions()
+acc1.show_summary()
+
+
