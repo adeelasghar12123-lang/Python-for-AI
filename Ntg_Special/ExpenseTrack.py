5c9ffe008c4b0421a33cf2edd80a750085d7bf0e
Adeel "Stoic" Asghar <264592599+adeelasghar12123-lang@users.noreply.github.com>
2026-09-22 23:42:58 +0500

    test

--- Ntg_Special/ExpenseTrack.py
+++ Ntg_Special/ExpenseTrack.py
@@ -26,32 +26,28 @@ def take_expense_input():
         expenses.append({"Expense Name" : expense_name , "Amount" : expense_amount})
 
 
-# DISPLAY EVERY EXPENSE
+
 def display_expenses(expenses):
     for ex in expenses:
         print(ex["Expense Name"],ex["Amount"])
 
-# TOTAL SPENDINGS
 def total_spendings(expenses):
     total_spendings = 0
     for ex in expenses:
         total_spendings += ex["Amount"]
     return total_spendings
 
-# AVERAGE EXPENSE
 def average():
     return total_spendings()/number_of_expenses
 
-# HIGHEST
+
 def Highest_expense(expenses):
     return max(expenses, key=lambda x: x["Amount"])
 
-# LOWEST
 def Lowest_expense(expenses):
     return min(expenses, key=lambda x: x["Amount"])
 
 
-#SPENDING LEVEL
 def Spending_level():
     total = total_spendings()
     if total < 5000 :
