cd025fe4e146170f5778e68ff7b97bb6601a8aae
Adeel "Stoic" Asghar <264592599+adeelasghar12123-lang@users.noreply.github.com>
2026-09-22 23:40:53 +0500

    jjbutt

--- Ntg_Special/bank_account.py
+++ Ntg_Special/bank_account.py
@@ -26,7 +26,7 @@ class bank_account:
 
 
     def deposit(self, amount):
-        if amount < 1 :
+        if amount < 5 :
             print("Enter a valid amount to deposit !")
             return
         self.balance += amount
