9f1749563a90e1731e01e4abb547411a9c361b7f
Adeel "Stoic" Asghar <264592599+adeelasghar12123-lang@users.noreply.github.com>
2026-09-23 23:58:18 +0500

    mewwwwwwwwwwwwwww

---
+++ idd5.py
@@ -0,0 +1,20 @@
+name = input("Enter name : ")
+count = 0
+
+while (count < 10):
+    print(name)
+    count += 2
+
+name = input("Enter name : ")
+count = 0
+
+while (count < 10):
+    print(name)
+    count += 2
+
+name = input("Enter name : ")
+count = 0
+
+while (count < 10):
+    print(name)
+    count += 2
