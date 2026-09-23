43ef8e1d4c06c0dee8ad9ad3ca945bbeb5cb5f8e
Adeel "Stoic" Asghar <264592599+adeelasghar12123-lang@users.noreply.github.com>
2026-09-23 23:57:55 +0500

    iddy4...

---
+++ iddy4.py
@@ -0,0 +1,26 @@
+name = input("Enter name : ")
+count = 0
+
+while (count < 10):
+    print(name)
+    count += 2
+
+name = input("Enter name : ")
+count = 0
+
+while (count < 10):
+    print(name)
+    count += 2
+
+name = input("Enter name : ")
+count = 0
+
+while (count < 10):
+    print(name)
+    count += 2
+name = input("Enter name : ")
+count = 0
+
+while (count < 10):
+    print(name)
+    count += 2
