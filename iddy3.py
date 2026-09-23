160137f828360308673118dd522ad55887c0e674
Adeel "Stoic" Asghar <264592599+adeelasghar12123-lang@users.noreply.github.com>
2026-09-23 23:57:08 +0500

    iddy3

---
+++ iddy3.py
@@ -0,0 +1,21 @@
+name = input("Enter name : ")
+count = 0
+
+while (count < 10):
+    print(name)
+    count += 2
+
+name = input("Enter name : ")
+count = 0
+
+while (count < 10):
+    print(name)
+    count += 2
+
+
+name = input("Enter name : ")
+count = 0
+
+while (count < 10):
+    print(name)
+    count += 2
