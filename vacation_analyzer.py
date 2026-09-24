cae4558793e40fa8a35d091ec00e7ef54654aa53
Adeel "Stoic" Asghar <264592599+adeelasghar12123-lang@users.noreply.github.com>
2026-09-24 13:57:13 +0500

    updated vacation recommender

--- vacation_analyzer.py
+++ vacation_analyzer.py
@@ -2,7 +2,7 @@ vacation_plan = [
     {
         "place" : "Paris",
         "return flight" : 200,
-        "hotel per day" : 100,
+        "hotel per day" : 20,
         "weekly car rental" : 200
 
     },
@@ -26,26 +26,51 @@ vacation_plan = [
         {
         "place" : "Mumbai",
         "return flight" : 450,
-        "hotel per day" : 10,
+        "hotel per day" : 30,
         "weekly car rental" : 70
 
     },
 ]
 
-def duration(time = input("Input time in weeks :") ,vacation_plan = vacation_plan ):
-    time = time.split()
-    weeks = int(time[0])
-    cost_per_week_min = vacation_plan[0]["return flight"] + (vacation_plan[0]["hotel per day"]*7) + vacation_plan[0]["weekly car rental"]
-    final = vacation_plan[0]["place"]
-    for dict in vacation_plan:
-        cost_per_week = dict["return flight"] + (dict["hotel per day"]*7) + dict["weekly car rental"]
-        if cost_per_week < cost_per_week_min:
-            cost_per_week_min = cost_per_week
-            final = dict["place"]
-    print(final)
+def get_cheapest_vacation(vacation_plan = vacation_plan ):
+    time = input("Input time duration for vacations(in days or in weeks or in months) : ") 
+    parts = time.split()
+    if len(parts) != 2:
+        print("Enter duration in valid format e.g. = (4 days / 2 weeks / 1 month)")
+        return
+    if parts[1].lower().startswith("d"):
+        days = int(parts[0])
+        weeks = 0
+        months = 0
+    elif parts[1].lower().startswith("w"):
+        weeks = int(parts[0])
+        days = 0
+        months = 0
+    elif parts[1].lower().startswith("m"):
+        months = int(parts[0])
+        days = 0
+        weeks = 0
+    else:
+        print("Invalid time unit. Use days, weeks, or months.")
+        return
+
+    days = days + 7*weeks + 30*months
+    weeks = days // 7
+    rent_weeks = weeks
+    if days % 7 != 0:
+        rent_weeks += 1
+
+    min_cost = float('inf')
+    final = ""
+    for plan in vacation_plan:
+        cost_per_week = plan["return flight"] + (plan["hotel per day"]*days) + (plan["weekly car rental"]*rent_weeks)
+        if cost_per_week < min_cost:
+            min_cost = cost_per_week
+            final = plan["place"]
+    print(f'Cheapest Destination : {final} ${min_cost}')
         
 
-duration()
+get_cheapest_vacation()
 
 
 
